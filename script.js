599a088105d96587ad7e11585cf6010964a0433b
KarthikShetty11 <kvshetty27@gmail.com>
2021-12-28 20:12:27 +0530

    DOM manipulation

---
+++ script.js
@@ -0,0 +1,17 @@
+/*
+const body = document.body
+//body.append("hello", ' Karthik')
+
+const div = document.createElement("div")
+//div.innerText = "namaste"
+div.innerHTML = "<strong>Hello</strong>"
+body.append(div)
+*/
+
+/*
+const div = document.querySelector("div")
+
+console.log(div.textContent)
+console.log(div.innerText)
+*/
+
